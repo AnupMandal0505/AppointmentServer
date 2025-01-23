3d5f133879cbae004fc2400bc1a8afe442d799d0
anurag <anurag@gmail.com>
2025-01-24 04:17:38 +0530

    router se

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path,include
-from user.views import *
-# from .views import AppointmentCreateAPIView,AppointmentDetailAPIView
 
-urlpatterns = [
-    path('login/',LoginAPI.as_view(), name='user-login'),
-    # path('appointments-create/', AppointmentCreateAPIView.as_view(), name='create_appointment'),  # Create appointment
+from rest_framework import routers
+from .views import LoginAPI,LogoutView
 
-]
+router = routers.DefaultRouter()
+router.register(r'login', LoginAPI, basename='login')
+router.register(r'logout', LogoutView, basename='logout')
+
+urlpatterns = router.urls
--- user/views.py
+++ user/views.py
@@ -3,64 +3,56 @@ from rest_framework.response import Response
 from user.serializers.LoginSerializer import LoginSerializer
 from user.serializers.UserSerializer import UserSerializer
 from django.contrib.auth import authenticate
-# from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.authtoken.models import Token
-from user.models import User
-# http://127.0.0.1:8000/api/login
-class LoginAPI(APIView):
-    def post(self, request):
+# from user.models import User
+from rest_framework import viewsets
+
+class LoginAPI(viewsets.ViewSet):
+    def create(self, request):
         try:
             data = request.data
-            print(data)
-            if not User.objects.filter(username=data['username'],role=data['role']).exists():
-                error_response = {
-                    'status': 400,
-                    'error': 'invalid_role',
-                    'message': 'Invalid role provided',
-                }
-                return Response(error_response, status=400)
-            
             serializer = LoginSerializer(data=data)
 
             if serializer.is_valid():
                 username = data['username']
                 password = data['password']
                 user = authenticate(username=username, password=password)
+
                 if user is None:
-                    error_response = {
-                        'status': 400,
-                        'error': 'invalid_password',
-                        'message': 'Invalid password',
-                        'data': {}
-                    }
-                    return Response(error_response, status=400)
+                    return self.error_response('invalid_password', 'Invalid password')
+
                 if Token.objects.filter(user=user).exists():
                     Token.objects.get(user=user).delete()
-                token=Token.objects.create(user=user)
-                # refresh = RefreshToken.for_user(user)
+                token = Token.objects.create(user=user)
 
-                success_response = {
+                return Response({
                     'user': UserSerializer(user, many=False).data,
                     'Token': token.key,
-                }
-                return Response(success_response)
-
+                })
 
             else:
-                error_response = {
-                    'status': 400,
-                    'error': 'invalid_data',
-                    'message': 'Invalid data',
-                    'data': {}
-                }
-                return Response(error_response, status=400)
+                return self.error_response('invalid_data', 'Invalid data', serializer.errors)
 
         except Exception as e:
-            print(e)
-            error_response = {
-                'status': 400,
-                'error': 'something_went_wrong',
-                'message': 'Something went wrong',
-                'data': {}
-            }
-            return Response(error_response, status=400)
+            return self.error_response('something_went_wrong', 'Something went wrong')
+
+    def error_response(self, error, message, data={}):
+        return Response({
+            'status': 400,
+            'error': error,
+            'message': message,
+            'data': data
+        }, status=400)
+    
+
+from rest_framework import status
+
+
+class LogoutView(viewsets.ViewSet):
+    def create(self, request):
+        try:
+            token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+            Token.objects.get(key=token).delete()
+            return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'message': 'Failed to log out'}, status=status.HTTP_400_BAD_REQUEST)
