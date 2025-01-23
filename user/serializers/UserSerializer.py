308e9cc21a5bc68c431f7b78447f712368225901
anurag <anurag@gmail.com>
2025-01-24 02:14:37 +0530

    update model

--- user/serializers/UserSerializer.py
+++ user/serializers/UserSerializer.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        exclude = ['password', 'id', 'last_login','is_superuser','is_staff','is_active','date_joined','groups','user_permissions']
+        exclude = ['password', 'last_login','is_superuser','is_staff','is_active','date_joined','groups','user_permissions']
         # fields = '__all__'
 
 
