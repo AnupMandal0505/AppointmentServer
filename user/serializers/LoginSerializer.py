54c152335a8ba033afc7cc2f32fb08b5c62d62ef
anurag <anurag@gmail.com>
2025-02-04 23:22:51 +0530

    S

--- user/serializers/LoginSerializer.py
+++
@@ -1,6 +0,0 @@
-# from rest_framework.serializers import serializers
-from rest_framework import serializers
-class LoginSerializer(serializers.Serializer):
-    username=serializers.CharField()
-    password= serializers.CharField()
-    role= serializers.CharField()
